10b2b8865bfc983f861ae2e48d8a39acde1baada
Jahlapeno1 <jimenezaot@gmail.com>
2024-06-11 17:52:39 -0400

    FINISHED

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,37 @@
-const resolvedWrapper = () => {
+const resolvedWrapper = (val) => {
+  const promise = new Promise((resolve, reject) => {
+      resolve(val)
+  })
+  return promise
 };
-
-const rejectedWrapper = () => {
+const rejectedWrapper = (str) => {
+  const promise = new Promise((resolve, reject) => {
+    reject(new Error(`${str}`))
+  })
+  return promise
 };
-
-const handleResolvedPromise = () => {
+const handleResolvedPromise = (promise) => {
+  // ???????????????????????????????????????????????????
+  return promise.then(val => {console.log(val)
+    return val
+  })
 };
-
-const handleResolvedOrRejectedPromise = () => {
+const handleResolvedOrRejectedPromise = (promise) => {
+  // ????????????????
+  return promise.then(val => {console.log(val)
+    return val
+  })
+  .catch(val => {console.error(`Your error message was: ${val.message}`)
+    return null
+  })
 };
-
-const pauseForMs = () => {
+const pauseForMs = (num) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, num);
+  })
+  return promise
 };
 
 module.exports = {
--- src/modify.js
+++ src/modify.js
@@ -1,36 +1,45 @@
 const crypto = require('crypto');
 
 const numsToRGBColor = ([color1, color2, color3]) => {
-  return `rgb(${color1}, ${color2}, ${color3})`;
+	return `rgb(${color1}, ${color2}, ${color3})`;
 };
 
-const getRandomBytes = () => new Promise((resolve, reject) => {
-  crypto.randomFill(new Uint8Array(3), (err, buffer) => {
-    if (err) return reject(err);
-    resolve([...buffer]);
-  });
-});
+const getRandomBytes = () =>
+	new Promise((resolve, reject) => {
+		crypto.randomFill(new Uint8Array(3), (err, buffer) => {
+			if (err) return reject(err);
+			resolve([...buffer]);
+		});
+	});
 
 const return4RandomColors = () => {
-  const colors = [];
-  return getRandomBytes()
-    .then(() => {
-    })
-    .then(() => {
-    })
-    .then(() => {
-    })
-    .then(() => {
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+	const colors = [];
+
+	return getRandomBytes()
+		.then((buffer1) => {
+			colors.push(numsToRGBColor(buffer1)); 
+			return getRandomBytes();
+		})
+		.then((buffer2) => {
+			colors.push(numsToRGBColor(buffer2));
+			return getRandomBytes();
+		})
+		.then((buffer3) => {
+			colors.push(numsToRGBColor(buffer3));
+			return getRandomBytes();
+		})
+		.then((buffer4) => {
+			colors.push(numsToRGBColor(buffer4));
+			return colors;
+		})
+		.catch((err) => {
+			console.error(err);
+			return [];
+		});
 };
 
-return4RandomColors().then(console.log);
-
 module.exports = {
-  numsToRGBColor,
-  getRandomBytes,
-  return4RandomColors,
+	numsToRGBColor,
+	getRandomBytes,
+	return4RandomColors,
 };
