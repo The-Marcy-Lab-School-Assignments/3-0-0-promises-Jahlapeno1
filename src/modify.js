fc66f2effd9d1b287477675514a2e241ce4a2abc
Jahlapeno1 <jimenezaot@gmail.com>
2024-06-11 18:11:05 -0400

    UPDATE

--- src/modify.js
+++ src/modify.js
@@ -16,20 +16,20 @@ const return4RandomColors = () => {
 	const colors = [];
 
 	return getRandomBytes()
-		.then((buffer1) => {
-			colors.push(numsToRGBColor(buffer1)); 
+		.then((color1) => {
+			colors.push(numsToRGBColor(color1)); 
 			return getRandomBytes();
 		})
-		.then((buffer2) => {
-			colors.push(numsToRGBColor(buffer2));
+		.then((color2) => {
+			colors.push(numsToRGBColor(color2));
 			return getRandomBytes();
 		})
-		.then((buffer3) => {
-			colors.push(numsToRGBColor(buffer3));
+		.then((color3) => {
+			colors.push(numsToRGBColor(color3));
 			return getRandomBytes();
 		})
-		.then((buffer4) => {
-			colors.push(numsToRGBColor(buffer4));
+		.then((color4) => {
+			colors.push(numsToRGBColor(color4));
 			return colors;
 		})
 		.catch((err) => {
